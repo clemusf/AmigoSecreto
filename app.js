2d7f155b5f8348bab5474c6da7b223431e5400c6
clemusf <clemusf@gmail.com>
2025-03-18 19:07:35 -0300

    creando el proyecto

---
+++ app.js
@@ -0,0 +1,51 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo =[];
+
+//alert("Entro");
+
+function agregarAmigo()
+{
+       
+ let ingresaAmigo = document.getElementById("amigo");
+ let nombreAmigo = ingresaAmigo.value;
+ 
+ if (!nombreAmigo){
+  alert("Debes ingresar el nombre de tu amigo");
+  return;
+ }
+  //alert("agrega amigo a la lista");
+  amigo.push(nombreAmigo);
+  console.log(amigo);
+  ingresaAmigo.value="";
+  ingresaAmigo.focus();
+  ordenarAmigos();
+}
+
+function ordenarAmigos(){
+
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML="";
+
+    //recorrer lista de amigos
+   for(let i=0; i < amigo.length; i++){
+    let item = createElement("li");
+    item.textContent= amigo[i];
+    listaAmigos.appendChild(item);
+
+   }
+}
+function sortearAmigo(){
+    //alert("sorteo");
+
+    if(amigo.length ===0){
+     alert("no hay amigos para el sorteo");
+     return;
+    }
+    let amigoSorteado =  amigo[Math.floor(Math.random() * amigo.length)]
+    let resultado= document.getElementById("resultado");
+    resultado.innerHTML=`El amigo ganador es :${amigoSorteado}`;
+
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML="";
+
+}
